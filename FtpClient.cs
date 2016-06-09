c5824ed28be6cb05c29b44e37d5bcdaebb7e3143
Jakub <melka9@gmail.com>
2016-06-10 00:01:00 +0200

    Parse FTP List directory

--- FtpClient.cs
+++ FtpClient.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace FTP_Image_Browser
 {
@@ -12,14 +14,17 @@ namespace FTP_Image_Browser
         }
         public void Connect()
         {
-            FtpListDirectory("UAVFORS/");
+            foreach(string dir in FtpListDirectory("UAVFORS/"))
+            {
+                Console.WriteLine(dir);
+            }
         }
         ~FtpClient()
         {
 
         }
 
-        private void FtpListDirectory(string remoteDir)
+        private List<string> FtpListDirectory(string remoteDir)
         {
             // Get the object used to communicate with the server.
             FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://"+ serverDomain_ + ":" + serverPort_ + "/" + remoteDir);
@@ -32,13 +37,32 @@ namespace FTP_Image_Browser
 
             Stream responseStream = response.GetResponseStream();
             StreamReader reader = new StreamReader(responseStream);
-            Console.WriteLine(reader.ReadToEnd());
+           // Console.WriteLine(reader.ReadToEnd());
+            List<string> dirListing = new List<string>();
+            while(true)
+            {
+                string dirLine = reader.ReadLine();
+                if (dirLine == null) break;
+                else
+                {
+                    //Console.WriteLine(dirLine);
+                    //Split directory line listing string//
+                    string[] dirParam = dirLine.Split(new char[]{' '}, StringSplitOptions.RemoveEmptyEntries);
+                    //Last parameter is the folder/file name - assume it soes not contain any space separators
+                    dirListing.Add(dirParam[dirParam.Length - 1]);
+                    //Console.WriteLine(dirParam[dirParam.Length-1]);
+                }
+            }
+            
 
             Console.WriteLine("Directory List Complete, status {0}", response.StatusDescription);
 
             reader.Close();
             response.Close();
+
+            return dirListing;
         }
+        //Server object
         //Default server parameters
         private string serverDomain_ = "srv40.ddns.net";
         private int serverPort_ = 2514;
